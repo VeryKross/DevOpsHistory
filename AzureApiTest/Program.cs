30d2b8906acb7df49f9ccf8b6005d5528fc3a31e
Ken Ross <86316102+MSKRoss@users.noreply.github.com>
2022-06-29 14:37:15 -0400

    Add project files.

---
+++ AzureApiTest/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using CommonLib;
+using Microsoft.TeamFoundation.Common;
+
+namespace AzureApiTest
+{
+    class Program
+    {
+        private static string _personalAccessToken;
+        private const string PatKey = "DevOpsHistoryInfo";
+        private const string OrgKey = "DevOpsOrgId";
+        private const string LocKey = "OutputLocation";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"DevOps History Reporter v1.5");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("");
+
+            if (args.Length == 1 && (args[0] == "?" || args[0].ToLower(CultureInfo.CurrentCulture) == "help"))
+            {
+                Console.WriteLine("Supported Parameters:");
+                Console.WriteLine();
+                Console.WriteLine("?/Help:       to see this message - congratulations!");
+                Console.WriteLine("Clear:        Used to clear some or all saved parameters (e.g. Clear, Clear org, Clear pat, Clear loc)");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("Note that you will need to supply a Personal Access Token (PAT) the first time you use the utility.");
+                Console.WriteLine("The PAT will be saved locally for subsequent use unless you opt out of storing it (in which case you'll be asked for it on each run).");
+
+                return;
+            }
+
+            // Get the current configuration file.
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            // Check to see if the user has asked for one or all of the stored values to be cleared.
+            if (args.Length > 0 && args[0].ToLower(CultureInfo.CurrentCulture) == "clear")
+            {
+                if (args.Length == 1)
+                {
+                    config.AppSettings.Settings.Remove(PatKey);
+                    config.AppSettings.Settings.Remove(OrgKey);
+                    config.AppSettings.Settings.Remove(LocKey);
+                }
+                else
+                {
+                    switch (args[1].ToLower(CultureInfo.CurrentCulture))
+                    {
+                        case "org":
+                            config.AppSettings.Settings.Remove(OrgKey);
+                            break;
+
+                        case "pat":
+                            config.AppSettings.Settings.Remove(PatKey);
+                            break;
+
+                        case "loc":
+                            config.AppSettings.Settings.Remove(LocKey);
+                            break;
+                    }
+                }
+            }
+
+
+            if (config.AppSettings.Settings[PatKey] == null)
+            {
+                Console.WriteLine("Enter your Personal Access Token (PAT):");
+                _personalAccessToken = Console.ReadLine();
+                Console.WriteLine($@"Type ""YES"" to remember your PAT.");
+                var saveIt = Console.ReadLine();
+
+                if (saveIt?.ToUpper(CultureInfo.CurrentCulture) == "YES")
+                {
+                    config.AppSettings.Settings.Add(PatKey, _personalAccessToken);
+                    config.Save();
+                }
+            }
+            else
+            {
+                _personalAccessToken = config.AppSettings.Settings[PatKey].Value;
+            }
+            
+            var taskId = 0;
+            var orgId = "";
+            var filePath = "";
+
+            if (config.AppSettings.Settings[OrgKey] == null)
+            {
+                Console.WriteLine("Enter your organization ID:");
+                orgId = Console.ReadLine();
+
+                Console.WriteLine($@"Type ""YES"" to remember your organization.");
+                var saveIt = Console.ReadLine();
+
+                if (saveIt?.ToUpper(CultureInfo.CurrentCulture) == "YES")
+                {
+                    config.AppSettings.Settings.Add(OrgKey, orgId);
+                    config.Save();
+                }
+            }
+            else
+            {
+                orgId = config.AppSettings.Settings[OrgKey].Value;
+            }
+
+            if (config.AppSettings.Settings[LocKey] == null)
+            {
+                Console.WriteLine(@"Enter the full path to the folder where you want the history file to be stored (e.g. 'C:\MyFiles'):");
+                filePath = Console.ReadLine();
+
+                Console.WriteLine($@"Type ""YES"" to remember your folder location.");
+                var saveIt = Console.ReadLine();
+
+                if (saveIt?.ToUpper(CultureInfo.CurrentCulture) == "YES")
+                {
+                    config.AppSettings.Settings.Add(LocKey, filePath);
+                    config.Save();
+                }
+            }
+            else
+            {
+                filePath = config.AppSettings.Settings[LocKey].Value;
+            }
+
+            if (!Directory.Exists(filePath))
+            {
+                try
+                {
+                    var di = Directory.CreateDirectory(filePath);
+                    Console.WriteLine($"New directory '{di.FullName}' has been created.");
+                    Console.WriteLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error while attempting to create output folder '{filePath}': {e.Message}");
+                    Console.WriteLine("Press ENTER to exit.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            Console.WriteLine("Enter the ID of the ADO Work Item to retrieve (e.g. 3421):");
+            var taskIdInput = Console.ReadLine();
+            Console.WriteLine();
+
+            if (!taskIdInput.IsNullOrEmpty())
+            {
+                try
+                {
+                    taskId = Convert.ToInt32(taskIdInput);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error while validating Work Item ID: {e.Message}");
+                    Console.WriteLine("A valid ADO work item ID (e.g. 3421) is required to continue.");
+                    Console.WriteLine("Press ENTER to exit.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("A valid ADO work item ID (e.g. 3421) is required to continue.");
+                Console.WriteLine("Press ENTER to exit.");
+                Console.ReadLine();
+                return;
+            }
+
+            var histDude = new HistoryTools(_personalAccessToken, taskId, orgId);
+
+            var changes = histDude.GetChangeHistory();
+
+            WriteList($@"{filePath}\{taskId}-Changes.txt", changes, taskId);
+        }
+        
+        /// <summary>
+        /// Write the formatted history scan results to the specified output file.
+        /// </summary>
+        /// <param name="fileName">The name of the file to create.</param>
+        /// <param name="listData">The list of key-value pairs representing the history scan results.</param>
+        /// <param name="workItemId">The ID of the work item this scan is for.</param>
+        public static void WriteList(string fileName, List<(string Key, string Value)> listData, int workItemId)
+        {
+            using (var writer = new StreamWriter(fileName))
+            {
+                DateTime theTime = DateTime.Now;
+                var timeZone = theTime.IsDaylightSavingTime()
+                    ? TimeZoneInfo.Local.DaylightName
+                    : TimeZoneInfo.Local.StandardName;
+
+                // List of keys that typically have very long string content and limited value in outputting.
+                List<string> skipKeys = new List<string>()
+                {
+                    "Custom.AssessmentOutcomeReason", "Custom.ModernizationStatusNotes", "System.History", "System.Description", "Custom.OptimizationStatusNotes", "Custom.ProgressNotes"
+                };
+
+                writer.WriteLine($"History as of {theTime.ToString(CultureInfo.CurrentCulture)} ({timeZone}) for ID {workItemId}");
+                writer.WriteLine("Values in [brackets] represent the value prior to this revision.");
+                writer.WriteLine(new string(':', 80));
+
+                foreach (var valuePair in listData)
+                {
+                    if (!skipKeys.Contains(valuePair.Key))
+                    {
+                        writer.WriteLine(valuePair.Key == "" ? valuePair.Value : $"{valuePair.Key}: {valuePair.Value}");
+                    }
+                    else
+                    {
+                        // Don't output these long strings, just indicate that a value was present (otherwise it makes reading the rest of the history difficult)
+                        writer.WriteLine($"{valuePair.Key}: ...long text skipped...");
+                    }
+                }
+            }
+
+            Console.WriteLine(new string('-',70));
+            Console.WriteLine($"Output saved in file: {fileName}");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ CommonLib/HistoryTools.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.TeamFoundation.Common;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace CommonLib
+{
+    public class HistoryTools
+    {
+        private const string ApiRootUri = "https://dev.azure.com/";
+        private const string ChangedByField = "System.ChangedBy";
+        private const string ChangedDateField = "System.ChangedDate";
+        private const string EmptyValueIndicator = "*empty*";
+
+        private Dictionary<string, string> _masterKeys;
+        private readonly List<string> _skipKeys;
+
+        private bool _valueTracking = false;
+        private string _trackField;
+        private string _firstVal = string.Empty;
+        private string _lastVal = string.Empty;
+
+        private string ApiUri { get; set; }
+        private int WorkItemId { get; set; }
+        private string PersonalAccessToken { get; set; }
+
+        public HistoryTools(string pat, int workItemId, string org)
+        {
+            _masterKeys = new Dictionary<string, string>();
+
+            PersonalAccessToken = pat;
+            WorkItemId = workItemId;
+            ApiUri = ApiRootUri + org;
+
+            _skipKeys = new List<string>() { "System.Rev", "System.Watermark", ChangedByField, ChangedDateField };
+        }
+
+
+        /// <summary>
+        /// Returns a list of Key/Value pairs documenting all field changes over the entire
+        /// history of the item. Optionally, writes out the detail field values from each revision.
+        /// </summary>
+        /// <param name="writeDetail">If TRUE, a text file will be created with the details from each revision.</param>
+        /// <param name="detailPath">Folder to write the revision detail files to.</param>
+        /// <returns>List of key/value pairs with field names and values representing changes in each revision.</returns>
+        public List<(string Key, string Value)> GetChangeHistory(bool writeDetail = false, string detailPath = "", string trackField = "")
+        {
+            Console.WriteLine($"Getting Change History for item {WorkItemId}...");
+
+            var result = new List<(string Key, string Value)>();
+            var sectionSeparator = new string('=', 60);
+            var headingSeparator = new string('-', 30);
+
+            _valueTracking = !string.IsNullOrEmpty(trackField);
+            _trackField = trackField;
+
+            var allRevisions = RetrieveRevisionHistory();
+
+
+            Console.WriteLine("Evaluating changes over each revision...");
+            // Look for changes from one revision to the next
+            foreach (var revisionItem in allRevisions)
+            {
+                if (writeDetail && !detailPath.IsNullOrEmpty())
+                {
+                    WriteRevision(revisionItem, detailPath);
+                }
+
+                var revHeading = new List<(string Key, string Value)>();
+                var delta = GetDelta(revisionItem.Fields, revisionItem.Rev > 1);
+
+                if (delta.Count > 0)
+                {
+                    if (!_valueTracking)
+                    {
+                        var changedDate = revisionItem.Fields.First(i => i.Key == ChangedDateField).Value + " UTC";
+                        var changedBy = (IdentityRef)revisionItem.Fields.First(i => i.Key == ChangedByField).Value;
+                        var revId = revisionItem.Rev == 1
+                            ? $"{revisionItem.Rev} - New Record"
+                            : revisionItem.Rev.ToString();
+
+                        if (revisionItem.Rev > 1)
+                            revHeading.Add(new("", sectionSeparator));
+
+                        revHeading.Add(new("Revision", revId));
+                        revHeading.Add(new("Changed Date", changedDate));
+                        revHeading.Add(new("Changed By", $"{changedBy.DisplayName} ({changedBy.UniqueName})"));
+                        revHeading.Add(new("", headingSeparator));
+
+                        result.AddRange(revHeading);
+                    }
+
+                    result.AddRange(delta);
+                }
+            }
+
+            Console.WriteLine($"Evaluated {allRevisions.Count} revisions to item {WorkItemId}.");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Compare the current revision field list against the last known state in the MasterKeys dictionary.
+        /// </summary>
+        /// <param name="fields">The complete list of fields (key/value pairs) for a given revision.</param>
+        /// <param name="includeOldValue">If TRUE, the delta output will include both the new value and the old.</param>
+        /// <returns></returns>
+        private List<(string Key, string Value)> GetDelta(IDictionary<string, object> fields, bool includeOldValue)
+        {
+            var result = new List<(string Key, string Value)>();
+
+            foreach (var field in fields)
+            {
+                if ((_valueTracking && field.Key == _trackField) || !_valueTracking)
+                {
+                    var newVal = field.Value.ToString();
+
+                    if (_valueTracking && string.IsNullOrEmpty(_firstVal)) _firstVal = newVal;
+                    if (_valueTracking) _lastVal = newVal;
+
+                    // Build up the master list of keys as we go.
+                    if (!_masterKeys.ContainsKey(field.Key))
+                    {
+                        _masterKeys.Add(field.Key, "");
+                    }
+
+                    var oldVal = _masterKeys[field.Key];
+
+                    if (!_skipKeys.Contains(field.Key))//&& _masterKeys.TryGetValue(field.Key, out var oldVal))
+                    {
+                        if (oldVal != newVal)
+                        {
+                            var outNew = !newVal.IsNullOrEmpty() ? newVal : EmptyValueIndicator;
+                            var outOld = !oldVal.IsNullOrEmpty() ? oldVal : EmptyValueIndicator;
+
+                            if (!_valueTracking)
+                            {
+                                //result.Add(new ($"Old: {field.Key}", oldVal));
+                                if (includeOldValue)
+                                {
+                                    result.Add(new($"{field.Key}", $"{outNew} [{outOld}]"));
+                                }
+                                else
+                                {
+                                    result.Add(new($"{field.Key}", $"{outNew}"));
+                                }
+                            }
+
+                            // Record the new value for this key.
+                            _masterKeys[field.Key] = newVal;
+                        }
+                    }
+
+                }
+            }
+
+            // This loop is necessary because removal of a field is sometimes represented by the field
+            // no longer being included in the list of fields within a rev. The first time we're not
+            // able to find a previously identified Key, we mark it as having been set to *empty*.
+            foreach (var masterKey in _masterKeys)
+            {
+                var oldVal = masterKey.Value;
+
+                if (!fields.ContainsKey(masterKey.Key) && masterKey.Value != EmptyValueIndicator)
+                {
+                    _masterKeys[masterKey.Key] = EmptyValueIndicator;
+                    if (!_valueTracking)
+                    {
+                        result.Add(new($"{masterKey.Key}", $"{EmptyValueIndicator} [{oldVal}]"));
+                    }
+                    else
+                    {
+                        _lastVal = EmptyValueIndicator;
+                    }
+                }
+            }
+
+            if (_valueTracking)
+            {
+                result.Add(new($"{_firstVal}", $"{_lastVal}"));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieve all history for the specified work item and return the complete key/value list.
+        /// </summary>
+        /// <returns></returns>
+        private List<WorkItem> RetrieveRevisionHistory()
+        {
+            Console.Write("Retrieving full history...");
+
+            var skipCount = 0;
+            var batch = 1;
+            var allRevisionItems = new List<WorkItem>();
+            var apiMax = 0;
+
+            try
+            {
+                VssConnection connection = new VssConnection(new Uri(ApiUri), new VssBasicCredential(string.Empty, PersonalAccessToken));
+                WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+                // Retrieve all of the revisions
+                while (skipCount >= 0)
+                {
+                    Console.Write($"{batch}...");
+
+                    // Retrieve up to a maximum of 200 revisions
+                    var revisionItems =
+                        witClient.GetRevisionsAsync(id: WorkItemId, skip: skipCount, expand: WorkItemExpand.All).Result;
+
+                    if(revisionItems.Count > 0) allRevisionItems.AddRange(revisionItems);
+
+                    // ADO APIs typically cap the # of records they're return on a single call (e.g. 200) so we'll assume that
+                    // the count we retrieve on the first call is the maximum we can retrieve and use that to calculate
+                    // a skip value so we can retrieve all records in multiple "batches" of max count records. In some
+                    // cases, all records will be received in the first call and so we'll receive nothing in the next.
+                    if (apiMax == 0) apiMax = revisionItems.Count;
+
+                    skipCount = revisionItems.Count == apiMax ? batch * apiMax : -1;
+                    batch += 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Exception in RetrieveHistory: {e.Message}");
+                Console.WriteLine();
+                //var errWork = new WorkItem();
+                //errWork.Fields = new Dictionary<string, object>
+                //{
+                //    { "error", e.Message }
+                //};
+                //allRevisionItems.Add(errWork);
+            }
+
+            Console.WriteLine("done.");
+            return allRevisionItems;
+        }
+
+        /// <summary>
+        /// This method can be used to "dump" the key/value pairs for a Revision so it can be
+        /// examined in more detail. This is triggered through parameters passed to the
+        /// GetChangeHistory method.
+        /// </summary>
+        /// <param name="workItem">This represents the state of a work item at a revision point, including all fields.</param>
+        /// <param name="folderName">This is the name of the folder to write the output file to.</param>
+        private void WriteRevision(WorkItem workItem, string folderName)
+        {
+            var fileName = Path.Combine(folderName, $"{WorkItemId}-{workItem.Rev}_Details.txt");
+            using (var writer = new StreamWriter(fileName))
+            {
+                // List of keys that typically have very long string content and limited value in outputting.
+                List<string> skipKeys = new List<string>()
+                {
+                    "Custom.AssessmentOutcomeReason", "Custom.ModernizationStatusNotes", "System.History", "System.Description", "Custom.OptimizationStatusNotes", "Custom.ProgressNotes"
+                };
+
+                writer.WriteLine($"Detail output for work item ID {WorkItemId}, revision {workItem.Rev}");
+                writer.WriteLine(new string('=', 80));
+
+                foreach (var valuePair in workItem.Fields)
+                {
+                    if (!skipKeys.Contains(valuePair.Key))
+                    {
+                        writer.WriteLine($"{valuePair.Key}: {valuePair.Value}");
+                    }
+                    else
+                    {
+                        // Don't output these long string, just indicate that a value was present
+                        writer.WriteLine($"{valuePair.Key}: ...long text skipped...");
+                    }
+                }
+            }
+        }
+    }
+}
